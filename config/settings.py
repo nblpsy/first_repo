13700131876261f4b86198a35164fba4f98cf5a8
keigo <Kimacchann@gmail.com>
2022-12-08 13:27:11 +0900

    lesson04

--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
